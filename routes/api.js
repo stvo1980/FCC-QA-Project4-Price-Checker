460bb4ad2a2cce6eba142b9c858f4e435705b484
Glitch (equable-polydactyl-apricot) <none>
2020-05-14 17:22:46 +0000

    🌂😀 Checkpoint ./routes/api.js:15407664/405

--- routes/api.js
+++ routes/api.js
@@ -14,27 +14,14 @@ var MongoClient = require('mongodb');
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 const fetch = require("node-fetch");
 
-/*
-async function getStock(stock){
-  const fetchResponce = await fetch("https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote")    
-  const {symbol , latestPrice} = await fetchResponce.json(); 
-  return {
-    symbol,
-    price:latestPrice
-  }
-  console.log("symbol", symbol)
-} 
-*/
+
 
 var stockDB = {};
 var arr = [] 
 async function getStock(stock){
   var fetchResponceFunc = await fetch("https://repeated-alpaca.glitch.me/v1/stock/"+stock+"/quote")    
   var {symbol, latestPrice} = await fetchResponceFunc.json(); 
-//  stockDataFunc = stockDataFunc.map(item=>{return{
- //   symbol:item.symbol,
- //   price:item.latestPrice
-//  }})
+
   return {stock:symbol, price:latestPrice };
   
 }
@@ -53,7 +40,7 @@ module.exports = function (app) {
     var combineArr = arr.concat(stockTwo,stockThree)
     
   
-    console.log(typeof stock)
+ //   console.log(typeof stock)
   if(typeof stock === "string") {
 //    res.json({symbol:stockData.symbol, price:stockData.latestPrice})
     stockDB= {stockData:stockOne};
@@ -65,14 +52,11 @@ module.exports = function (app) {
      
      console.log("stockDB",stockDB)
     res.send(stockDB)}
- //   console.log("stock cond 2", stock[0])
-//    console.log("fetchResponce",stockData)
+ 
    
 //    res.json({symbol:stockData.symbol, price:stockData.latestPrice})
    
-  
-  
-  
+    
   
   
   });
