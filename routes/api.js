3195042f8743f506f161ea0ec07938569a852d07
Glitch (equable-polydactyl-apricot) <none>
2020-05-14 01:46:20 +0000

    💏🎹 Checkpoint ./routes/api.js:15407664/8319

--- routes/api.js
+++ routes/api.js
@@ -8,30 +8,82 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+// var expect = require('chai').expect;
+// var MongoClient = require('mongodb');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-async function getStock(stock){
-  const fetchResponce = await fetch("https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote")    
-  const {symbol , latestPrice} = await fetchResponce.json(); 
+
+// our inner DB
+const stocksDB = {};
+
+async function getStock(stock) {
+  const fetchResponse = await fetch(`https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote`);
+  const { symbol, latestPrice } = await fetchResponse.json();
+  
   return {
     symbol,
-    price:latestPrice
-  }
-} 
+    price: `${latestPrice}`
+  };
+}
 
+async function addToStocksDB(stock, like) {
+  const returnedStock = await getStock(stock);
+  const { symbol, price } = returnedStock;
+
+  if(symbol) {
+    if(stocksDB.hasOwnProperty(symbol)) {
+      stocksDB[symbol] = {
+        price,
+        like: like ? stocksDB[symbol].like + 1 : stocksDB[symbol].like
+      }
+    } else {
+      stocksDB[symbol] = {
+        price,
+        like: like ? 1 : 0
+      }
+    }
+  }
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res){
-      const {stock, like} = req.query;
-  const googStock = await getStock('goog')
-   
-    res.json(googStock)
-    });
+      const { stock, like } = req.query;
     
+      const stockKeys = [];
+  
+      if(stock) {
+        if(typeof stock === 'string') {
+          await addToStocksDB(stock, like);
+          
+          stockKeys.push(stock.toUpperCase());
+        } else {
+          // is an array
+          for(let i=0; i<stock.length; i++) {
+            await addToStocksDB(stock[i], like);
+            
+            stockKeys.push(stock[i].toUpperCase());
+          }
+        }
+      }
+    
+      const result = (stockKeys.length === 1) ? 
+            {...stocksDB[stockKeys[0]], stock: stockKeys[0] }
+      : Object.keys(stocksDB)
+        .filter(key => stockKeys.includes(key))
+        .map(key => ({
+          stock: key,
+          ...stocksDB[key]
+        }));
+    
+    
+      res.json({
+        stockData: result
+      });
+    });
 };
+
+// {"stockData":{"stock":"GOOG","price":"786.90","likes":1}}
+// {"stockData":[{"stock":"MSFT","price":"62.30","rel_likes":-1},{"stock":"GOOG","price":"786.90","rel_likes":1}]}
