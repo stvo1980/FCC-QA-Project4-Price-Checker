d1b20e9fbb9dd6370b5251dd83f897d91ab056aa
Glitch (equable-polydactyl-apricot) <none>
2020-05-14 00:12:39 +0000

    🎰👴 Checkpoint ./package.json:15407664/1255 ./routes/api.js:15407664/245

--- routes/api.js
+++ routes/api.js
@@ -12,12 +12,15 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
+const fetch = require("node-fetch");
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      const {stock, like} = req.query;
+ const fetchResponce = fetch("")    
+    
+    //  console.log(req.params);
     });
     
 };
