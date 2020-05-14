041b9cb09c957a5435c2bdeed9de63f473856251
Glitch (equable-polydactyl-apricot) <none>
2020-05-14 16:58:49 +0000

    🏥🚧 Checkpoint ./routes/api.js:15407664/316

--- routes/api.js
+++ routes/api.js
@@ -26,8 +26,8 @@ async function getStock(stock){
 } 
 */
 
-const stockDB = {};
-
+var stockDB = {};
+var arr = [] 
 async function getStock(stock){
   var fetchResponceFunc = await fetch("https://repeated-alpaca.glitch.me/v1/stock/"+stock+"/quote")    
   var {symbol, latestPrice} = await fetchResponceFunc.json(); 
@@ -46,15 +46,18 @@ module.exports = function (app) {
     .get(async function (req, res){
       const {stock, like} = req.query;
 //    const fetchResponce = await fetch("https://repeated-alpaca.glitch.me/v1/stock/"+stock+"/quote")    
-  var stockFirst = await getStock(stock);
-    var stockSecond = await getStock(stock[0]);
-    var stockThird = await getStock(stock[1])
+  var stockOne = await getStock(stock);
+    var stockTwo = await getStock(stock[0]);
+    var stockThree = await getStock(stock[1])
     
+    var combineArr = arr.concat(stockTwo,stockThree)
     
-    console.log("stockFirst getStock",stockFirst);
-    console.log("stock2 getStock",stockSecond);
-    console.log("stock2",stockThird);
+  //  console.log("stockFirst getStock",stockOne);
+ //   console.log("stock2 getStock",stockTwo);
+ //   console.log("stock2",stockThree);
     
+    stockDB= {stockData:combineArr};
+    console.log("stockDB",stockDB);
   //  const {symbol , latestPrice, open,close} = await fetchResponce.json(); 
 //    var stockData = await fetchResponce.json(); 
 
@@ -63,11 +66,12 @@ module.exports = function (app) {
     
 //  const googStock = await getStock('goog')
  //   console.log(typeof stock)
-//  if(typeof stock === "string") {
+  if(typeof stock === "string") {
 //    res.json({symbol:stockData.symbol, price:stockData.latestPrice})
-//    console.log("cond stock", stock)
-//  } else 
- //   console.log("stock cond 2", stock[0])
+//    
+    console.log("cond stock", stockOne)
+  } else 
+    console.log("stockDB",stockDB)
  //   console.log("stock cond 2", stock[0])
 //    console.log("fetchResponce",stockData)
    
