3d1affaba425a5753d6811ee6a78b166490bf77b
Glitch (equable-polydactyl-apricot) <none>
2020-05-14 00:36:40 +0000

    🏗🚆 Checkpoint ./routes/api.js:15407664/539

--- routes/api.js
+++ routes/api.js
@@ -13,15 +13,25 @@ var MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 const fetch = require("node-fetch");
+
+async function getStock(stock){
+  const fetchResponce = await fetch("https://repeated-alpaca.glitch.me/v1/stock/${stock}/quote")    
+  const {symbol , latestPrice} = await fetchResponce.json(); 
+  return {
+    symbol,
+    price:latestPrice
+  }
+} 
+
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res){
       const {stock, like} = req.query;
- const fetchResponce = await fetch("https://repeated-alpaca.glitch.me/v1/stock/GOOG/quote")    
-  const respData = await fetchResponce.json();  
+  const googStock = await getStock('goog')
     //  console.log(req.params);
-    res.json(respData)
+    res.json(googStock)
     });
     
 };
